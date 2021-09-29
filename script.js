949d787f02ad9f090512ca2bccd6f33a5993946f
shubham-tyagi <raj.tyagi2000@gmail.com>
2021-09-29 18:36:48 +0530

    initial commit,feat : whole app completed

---
+++ script.js
@@ -0,0 +1,59 @@
+const initialPrice = document.querySelector("#initialPrice");
+const noOfStocks = document.querySelector("#noOfStocks");
+const currentPrice = document.querySelector("#currentPrice");
+const message = document.querySelector(".message");
+const btnSubmit = document.querySelector("#btnSubmit");
+console.log("hello");
+btnSubmit.addEventListener("click", btnSubmitHandler);
+
+function btnSubmitHandler() {
+	var iniPrice = Number(initialPrice.value);
+	var currPrice = Number(currentPrice.value);
+	var noStocks = Number(noOfStocks.value);
+	if (iniPrice == "" || currPrice == "" || noOfStocks == "") {
+		alert("enter All the values");
+		return;
+	}
+	if (noStocks < 0) {
+		alert("quantity of stocks should be greater than 0");
+		return;
+	}
+	if (iniPrice < 0) {
+		alert("Initial Price should be greater than 0");
+		return;
+	}
+	if (currPrice < 0) {
+		alert("Current Price should be greater than 0");
+		return;
+	}
+	console.log(typeof iniPrice);
+	if (iniPrice > currPrice) {
+		var loss = (iniPrice - currPrice) * noStocks;
+		var lossPercentage = (loss / iniPrice) * 100;
+		message.innerHTML =
+			"Your loss is : " + loss + " your loss percentage is : " + lossPercentage;
+		message.style.backgroundColor = "red";
+		message.style.border = "1px solid black";
+		message.style.padding = "1rem";
+		message.style.borderRadius = "1rem";
+	} else if (currPrice > iniPrice) {
+		var profit = (currPrice - iniPrice) * noStocks;
+		var profitPercentage = (profit / iniPrice) * 100;
+		// console.log(profitPercentage);
+		message.innerHTML =
+			"Your profit is : " +
+			profit +
+			" your profit percentage is : " +
+			profitPercentage;
+		message.style.backgroundColor = "green";
+		message.style.border = "1px solid black";
+		message.style.padding = "1rem";
+		message.style.borderRadius = "1rem";
+	} else {
+		message.innerHTML = "no pain no gain and no gain no pain ";
+		message.style.backgroundColor = "grey";
+		message.style.border = "1px solid black";
+		message.style.padding = "1rem";
+		message.style.borderRadius = "1rem";
+	}
+}
